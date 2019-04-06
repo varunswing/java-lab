e92277ba6223e81bcd2df0029052319f7860dc27
varunswing <varuns939@gmail.com>
2019-04-06 23:55:54 +0530

    Create GeometricObject.java

---
+++ GeometricObject.java
@@ -0,0 +1,13 @@
+abstract class GeometricObject
+{
+    String color;
+    float weight;
+    GeometricObject();
+    {
+        color="white"
+        weight=1.0;
+    }
+    String getcolor()
+    {returncolor;}
+    
+}
